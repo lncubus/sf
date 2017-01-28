e51a953b4a1ce1bff74ba2db7fa583cd72c6514c
lncubus <randirbox@gmail.com>
2017-01-28 21:54:16 +0300

    Initial implementation

---
+++ simTest/AdamsBashforth.cs
@@ -0,0 +1,82 @@
+﻿using ValueType = System.Numerics.Complex;
+
+using System;
+using System.Collections.Generic;
+
+namespace simTest
+{
+	public class AdamsBashforth : ISolver<ValueType>
+	{
+		// call X' = F(t, X)
+		public Func<double, ValueType, ValueType> F { get; private set; }
+		public double t { get; private set; }
+		public double dt { get; private set; }
+		public ValueType X { get; private set; }
+		public ValueType dXdt { get { return pF[0]; } }
+		private readonly ValueType[] pF;
+		private static readonly double[] k =
+		{
+			1901d/720d,  // * F(t_n, X_n)
+			-1387d/360d, // * F(t_n-1, X_n-1)
+			109d/30d,    // * F(t_n-2, X_n-2)
+			-637d/360d,  // * F(t_n-3, X_n-3)
+			251d/720d,   // * F(t_n-4, X_n-4)
+		};
+
+		public AdamsBashforth(Func<double, ValueType, ValueType> Function, double step, double t0, ValueType X0)
+		{
+			F = Function;
+			dt = step;
+			t = t0;
+			X = X0;
+			pF = new ValueType[4];
+			Start();
+		}
+
+		/// <summary>
+		/// Running <see cref="RungeKutta"/> to get previous F(t,X) values 
+		/// </summary>
+		private void Start()
+		{
+			RungeKutta rk = new RungeKutta
+			{
+				F = this.F,
+				t = this.t,
+				X = this.X,
+				dt = -dt
+			};
+			rk.Step();
+			rk.Step();
+			// F(t_-1, X_-1)
+			pF[0] = rk.dXdt;
+			rk.Step();
+			// F(t_-2, X_-2)
+			pF[1] = rk.dXdt;
+			rk.Step();
+			// F(t_-3, X_-3)
+			pF[2] = rk.dXdt;
+			rk.Step();
+			// F(t_-4, X_-4)
+			pF[3] = rk.dXdt;
+		}
+
+		/// <summary>
+		/// X_n+1 = X_n + dt*(
+		///   1901/720 * F(t_n, X_n)
+		///  -1387/360 * F(t_n-1, X_n-1)
+		///  + 109/30  * F(t_n-2, X_n-2)
+		///  - 637/360 * F(t_n-3, X_n-3)
+		///  + 251/720 * F(t_n-4, X_n-4)
+		/// )
+		/// </summary>
+		public void Step()
+		{
+			var Fn = F(t, X);
+			var sum = k[0] * Fn + k[1] * pF[0] + k[2] * pF[1] + k[3] * pF[2] + k[4] * pF[3];
+			t += dt;
+			X += dt * sum;
+			pF[3] = pF[2]; pF[2] = pF[1]; pF[1] = pF[0]; pF[0] = Fn;
+		}
+	}
+}
+
---
+++ simTest/Program.cs
@@ -0,0 +1,99 @@
+﻿using ValueType = System.Numerics.Complex;
+using System;
+
+namespace simTest
+{
+	class MainClass
+	{
+		int counter = 0;
+
+		void Run()
+		{
+			Console.WriteLine("Testing RK4...");
+			var sw = new System.Diagnostics.Stopwatch();
+			sw.Start();
+			// integ_0^T cos t dt = sin T 
+			//TestRungeKutta(100, 0.1, 0, 0, (t, X) => Math.Cos(t), t => Math.Sin(t));
+			TestRungeKutta(1000, 0.1, 0, ValueType.ImaginaryOne, (t, X) => ValueType.Cos(t), t => ValueType.Sin(t) + ValueType.ImaginaryOne);
+			TestRungeKutta(100000, 0.001, 0, ValueType.ImaginaryOne, (t, X) => ValueType.Cos(t), t => ValueType.Sin(t) + ValueType.ImaginaryOne);
+				//t => Math.Sin(t));
+			// x' = -x, x(0) = 1 => x = e^-t
+			//TestRungeKutta(100, 0.1, 0, 1, (t, X) => -X, t => Math.Exp(-t));
+			var X0 = ValueType.One + ValueType.ImaginaryOne;
+			TestRungeKutta(1000, 0.1, 0, X0, (t, X) => X, t => X0*Math.Exp(t)); //t => Math.Sin(t));
+			TestRungeKutta(100000, 0.001, 0, X0, (t, X) => X, t => X0*Math.Exp(t)); //t => Math.Sin(t));
+			sw.Stop();
+			Console.WriteLine("{0} steps total {1} ms ave {2} mus",
+				counter, sw.ElapsedMilliseconds, sw.ElapsedMilliseconds*1000.0/counter);
+			Console.WriteLine("Testing AB4...");
+			counter = 0;
+			sw.Restart();
+			// integ_0^T cos t dt = sin T 
+			TestAdamsBashforth(1000, 0.1, 0, ValueType.ImaginaryOne, (t, X) => ValueType.Cos(t), t => ValueType.Sin(t) + ValueType.ImaginaryOne);
+			TestAdamsBashforth(100000, 0.001, 0, ValueType.ImaginaryOne, (t, X) => ValueType.Cos(t), t => ValueType.Sin(t) + ValueType.ImaginaryOne);
+			// x' = -x, x(0) = 1 => x = e^-t
+			//TestAdamsBashforth(100, 0.1, 0, 1, (t, X) => -X, t => Math.Exp(-t));
+			TestAdamsBashforth(1000, 0.1, 0, X0, (t, X) => X, t => X0*Math.Exp(t));
+			TestAdamsBashforth(100000, 0.001, 0, X0, (t, X) => X, t => X0*Math.Exp(t)); //t => Math.Sin(t));
+			sw.Stop();
+			Console.WriteLine("{0} steps total {1} ms ave {2} mus",
+				counter, sw.ElapsedMilliseconds, sw.ElapsedMilliseconds*1000d/counter);
+		}
+
+		void TestRungeKutta(int n, double dt, double t0, ValueType X0,
+			Func<double, ValueType, ValueType> F, Func <double, ValueType> E)
+		{
+			RungeKutta rk = new RungeKutta
+			{
+				F = F,
+				t = t0,
+				dt = dt,
+				X = X0,
+			};
+			TestSolver(rk, n, E);
+		}
+	
+		void TestAdamsBashforth(int n, double dt, double t0, ValueType X0,
+			Func<double, ValueType, ValueType> F, Func <double, ValueType> E)
+		{
+			AdamsBashforth ab = new AdamsBashforth
+				(Function : F, step : dt, t0 : t0, X0 : X0);
+			TestSolver(ab, n, E);
+		}
+
+		void TestSolver(ISolver<ValueType> solver, int n, Func <double, ValueType> E)
+		{
+			var tXs = solver.Evaluate(n);
+			var k = n / 5;
+			var i = 0;
+			var maxerror = 0d;
+			foreach (var tX in tXs)
+			{
+				bool report = (k < 2 || i++ % k == 0 || i == n);
+				var t = tX.Item1;
+				var X = tX.Item2;
+				if (report)
+					Console.Write("{0:0.###}\t{1}", t, X);
+				if (E != null)
+				{
+					var X1 = E(t);
+					var error = X == X1 ? 0.0 : (X-X1).Magnitude/(X.Magnitude+X1.Magnitude);
+					if (error > maxerror)
+						maxerror = error;
+					if (report)
+						Console.Write("\t{0}\t{1}", X1, error);
+				}
+				if (report)
+					Console.WriteLine();
+			}
+			counter += n;
+			var koef = maxerror / Math.Pow(solver.dt, 4);
+			Console.WriteLine("max. err = {0}, koef = {1}", maxerror, koef);
+		}
+
+		public static void Main (string[] args)
+		{
+			new MainClass().Run();
+        }
+	}
+}
