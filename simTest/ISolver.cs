fa0d2d4c51b66f97cba4ec0e3fa13f9d9fd9fd59
lncubus <randirbox@gmail.com>
2017-01-28 21:54:16 +0300

    Initial implementation

---
+++ simTest/ISolver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace simTest
+{
+	public interface ISolver<ValueType>
+	{
+		Func<double, ValueType, ValueType> F { get; }
+		double t { get; }
+		double dt { get; }
+		ValueType X { get; }
+		ValueType dXdt { get; }
+
+		void Step();
+	}
+
+	public static class SolverExtensions
+	{
+		public static IEnumerable<Tuple<double, ValueType>> Evaluate<ValueType>(this ISolver<ValueType> that, int n)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				that.Step();
+				yield return Tuple.Create(that.t,that.X);
+			}
+		}
+	}
+}
+
---
+++ simTest/RungeKutta.cs
@@ -0,0 +1,29 @@
+using ValueType = System.Numerics.Complex;
+
+using System;
+using System.Collections.Generic;
+
+namespace simTest
+{
+	public class RungeKutta : ISolver<ValueType>
+	{
+		// call X' = F(t, X)
+		public Func<double, ValueType, ValueType> F { get; set; }
+		public double t { get; set; }
+		public double dt { get; set; }
+		public ValueType X { get; set; }
+		public ValueType dXdt { get; private set; }
+
+		public void Step()
+		{
+			dXdt = F(t, X);
+			var dX1 = dt*dXdt;
+			var dX2 = dt*F(t + dt/2.0, X + dX1/2.0);
+			var dX3 = dt*F(t + dt/2.0, X + dX2/2.0);
+			var dX4 = dt*F(t + dt, X + dX3);
+			t += dt;
+			X += (dX1 + 2.0 * (dX2 + dX3) + dX4)/6.0;
+		}
+	}
+}
+
