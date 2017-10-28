acf3b219a75510f3a0034b7c1a835e2bedab545d
lncubus <randirbox@gmail.com>
2017-10-28 20:47:55 +0200

    second sample - empty control

---
+++ Sample2/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+using Vectors;
+
+namespace Sample
+{
+	public partial class Form1 : Form
+	{
+		private SpaceView spaceView;
+		//  private TextBox textBox1;
+		private Random random = new Random ();
+
+		Vector3 RandomVector (double a = 1)
+		{
+			return new Vector3 {
+				X = a * (random.NextDouble () - 0.5),
+				Y = a * (random.NextDouble () - 0.5),
+				Z = a * (random.NextDouble () - 0.5),
+			};
+		}
+
+		public Form1 ()
+		{
+			Font = new Font (Font.FontFamily, 1.5F * Font.Size);
+			InitializeComponent ();
+
+			// 
+			// viewContainer1
+			// 
+			this.spaceView = new SpaceView () {
+				BackColor = System.Drawing.Color.Black,
+				BorderStyle = System.Windows.Forms.BorderStyle.None,
+				Dock = System.Windows.Forms.DockStyle.Fill,
+				ForeColor = System.Drawing.Color.White,
+				Location = new System.Drawing.Point (0, 0),
+				Name = "space",
+				Size = new System.Drawing.Size (292, 272),
+				TabIndex = 0,
+			};
+			spaceView.MouseDown += spaceView_MouseDown;
+			spaceView.MouseUp += spaceView_MouseUp;
+			spaceView.MouseMove += spaceView_MouseMove;
+			this.Controls.Add (spaceView);
+			spaceView.BringToFront ();
+
+//			spaceView.WorldMatrix = Matrix4x4.CreatePerspective (1, 1, 0.5, 3);
+				
+			//CreateLookAt(Vector3.Zero, -Vector3.UnitZ, Vector3.UnitY);
+
+			spaceView.SizeChanged += (object sender, EventArgs e) =>
+			{
+				spaceView.DeviceOrigin = new Point {
+					X = spaceView.ClientSize.Width / 2,
+					Y = spaceView.ClientSize.Height / 2
+				};
+			};
+
+			this.WindowState = FormWindowState.Maximized;
+		}
+
+		bool rotating;
+		Point origin;
+		Matrix4x4 world;
+
+		private void spaceView_MouseDown (object sender, MouseEventArgs e)
+		{
+			if (e.Button != MouseButtons.Left)
+				return;
+			origin = spaceView.PointToScreen (e.Location);
+			world = spaceView.WorldMatrix;
+			rotating = true;
+			spaceView.Cursor = Cursors.NoMove2D;
+		}
+
+		private void spaceView_MouseMove (object sender, MouseEventArgs e)
+		{
+			if (e.Button != MouseButtons.Left || !rotating)
+			{
+				spaceView.Cursor = Cursors.Default;
+				return;
+			}
+			Point current = spaceView.PointToScreen (e.Location);
+			double height = Math.Min (SpaceView.Resolution.Width, SpaceView.Resolution.Height);
+			Vector3 axis = new Vector3 {
+				X = (current.Y - origin.Y) / height,
+				Y = (current.X - origin.X) / height,
+				Z = 0,
+			};
+			double angle = axis.Length ();
+			if (angle * height < 3)
+				return;
+			axis /= angle;
+			var rotation = Matrix4x4.CreateFromAxisAngle (axis, Math.PI * angle);
+			spaceView.WorldMatrix = rotation * world;
+		}
+
+		private void spaceView_MouseUp (object sender, MouseEventArgs e)
+		{
+			rotating = false;
+			spaceView.Cursor = Cursors.Default;
+		}
+    }
+}
---
+++ Sample2/SpaceView.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vectors;
+
+namespace Sample
+{
+    public partial class SpaceView : UserControl
+    {
+        protected PointF deviceScale = new PointF(Dpi.X, Dpi.Y);
+        protected Point deviceOrigin = new Point(0, 0);
+
+        protected Matrix4x4 worldMatrix = Matrix4x4.Identity;
+        protected bool hideNegative = false;
+        protected bool perspectiveProjection = false;
+
+        public static readonly Point Dpi;
+        public static readonly Size Resolution;
+
+        public SpaceView() : base()
+        {
+            AutoSize = false;
+            AutoScroll = false;
+        }
+
+        static SpaceView()
+        {
+            using (Graphics graphics = Graphics.FromHwnd(IntPtr.Zero))
+            {
+                Dpi = new Point
+                {
+                    X = (int)graphics.DpiX,
+                    Y = (int)graphics.DpiY,
+                };
+            }
+            Resolution = Screen.PrimaryScreen.Bounds.Size;
+        }
+
+        public Point DeviceOrigin
+        {
+            get
+            {
+                return deviceOrigin;
+            }
+            set
+            {
+                deviceOrigin = value;
+                UpdateLayout();
+            }
+        }
+
+        public PointF DeviceScale
+        {
+            get
+            {
+                return deviceScale;
+            }
+            set
+            {
+                deviceScale = value;
+                UpdateLayout();
+            }
+        }
+
+        /// <summary>
+        /// https://en.wikipedia.org/wiki/Camera_matrix
+        /// </summary>
+        public Matrix4x4 WorldMatrix
+        {
+            get
+            {
+                return worldMatrix;
+            }
+            set
+            {
+                worldMatrix = value;
+                UpdateLayout();
+            }
+        }
+
+        private void UpdateLayout()
+        {
+            // TODO: IMPLEMENT
+            // throw new NotImplementedException();
+        }
+    }
+}
